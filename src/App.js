1cc918ee1295086e10fe2eb0ba114383aff74cd7
himanshusalal <himanshusalal517@gmail.com>
2026-05-21 11:46:54 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <Switch>
+
+        <Route exact path="/" component={ItemList} />
+
+        <Route
+          path="/items/:id"
+          component={ItemDetail}
+        />
+
+      </Switch>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/ItemDetail.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const items = [
+  {
+    id: 1,
+    name: "Item 1",
+    description: "Description for Item 1",
+  },
+  {
+    id: 2,
+    name: "Item 2",
+    description: "Description for Item 2",
+  },
+  {
+    id: 3,
+    name: "Item 3",
+    description: "Description for Item 3",
+  },
+];
+
+function ItemDetail(props) {
+
+  const id = props.match.params.id;
+
+  const item = items.find(
+    (item) => item.id === Number(id)
+  );
+
+  return (
+    <div>
+
+      <h1>{item.name}</h1>
+
+      <p>{item.description}</p>
+
+    </div>
+  );
+}
+
+export default ItemDetail;
